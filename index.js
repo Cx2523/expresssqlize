6c087468b83c4427c167a14c8e97862c98eddd71
Christopher Caldwell <clc523@gmail.com>
2018-07-07 14:16:08 -0600

    Started express and sqlize CRUD functions

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
+app.use(bodyParser.json());
 
 app.listen(3333, () => console.log('listening on port 3333'));
+
+const dbOp = require('./sequelize');
+
+app.get('/', (req, res) => {
+    dbOp.getAllExercises().then(result => res.send(result));
+});
+
+app.get('/:id', (req, res) => {
+    dbOp.getExerciseById(req.params.id).then(result => res.send(result));
+});
+
+app.post('/', (req, res) => {
+    dbOp.createNewExercise(req.body).then(() => res.send('Exercise inserted.'));
+});
+
+app.delete('/:id', (req, res) => {
+    dbOp.deleteExerciseById(req.params.id).then(() => res.send('Exercise Deleted'));
+});
---
+++ sequelize.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('Test2', 'postgres', 'password', {
+    dialect: 'postgres'
+});
+
+const Exercise = sequelize.define('exercise', {
+    Name: Sequelize.STRING,
+    Description: Sequelize.TEXT, 
+    Category: Sequelize.ENUM('Aerobic', 'Anaerobic', 'Flexibility', 'Stability'),
+    Metric1: Sequelize.ENUM('Weight', 'Reps', 'Time'),
+    Metric2: Sequelize.ENUM('Weight', 'Reps', 'Time')
+});
+
+// Exercise.sync();
+
+function getAllExercises() {
+    return Exercise.findAll().then(exercise => {
+        return exercise;
+    });
+}
+
+function createNewExercise(newExercise) {
+    return Exercise.create({
+        Name: newExercise.Name,
+        Description: newExercise.Description,
+        Category: newExercise.Category,
+        Metric1: newExercise.Metric1,
+        Metric2: newExercise.Metric2
+    });
+}
+
+function deleteExerciseById(id) {
+    return Exercise.destroy({
+        where: {
+            id: id
+        }
+    });
+}
+
+function getExerciseById(id) {
+    return Exercise.findById(id);
+}
+
+
+module.exports = {
+    getAllExercises,
+    createNewExercise,
+    deleteExerciseById,
+    getExerciseById,
+    // updateExerciseById
+}
