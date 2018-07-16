314cf1203eda4be6b4cfa265a944d92c318855f5
Christopher Caldwell <clc523@gmail.com>
2018-07-16 07:41:37 -0600

    db test

--- models/index.js
+++ models/index.js
@@ -17,8 +17,8 @@ if (process.env.DATABASE_URL) {
   var sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
+    // port:     match[4],
+    // host:     match[3],
     logging:  true
   });
 } else {
